8adc3e9d4fd29ad45df2dcf531931befaf9a949a
bandery911 <liuyantx3@163.com>
2019-07-11 23:28:10 +0800

    功能函数合集

---
+++ src/com/bandery/random/RandomChineseAddress.java
@@ -0,0 +1,55 @@
+package com.bandery.random;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * 随机生成简单的中国地址，如：浙江省杭州市仙山东路99号
+ * 门牌号最大99
+ */
+public class RandomChineseAddress {
+    private static final Random random = new Random(System.currentTimeMillis());
+
+    /**
+     * 随机生成一个中国地址
+     */
+    public static String randomChineseAddress() throws Exception{
+        BufferedReader provAndCityReader = new BufferedReader(new FileReader(new File("resources/chineseProvinceAndCity")));
+        BufferedReader roadReader = new BufferedReader(new FileReader(new File("resources/commonStreetName")));
+        ArrayList<String> cities = new ArrayList<>();
+        ArrayList<String> roads = new ArrayList<>();
+        String line = provAndCityReader.readLine();
+        while (line != null){
+            cities.add(line.trim());
+            line = provAndCityReader.readLine();
+        }
+        line = roadReader.readLine();
+        while (line != null){
+            roads.add(line.trim());
+            line = roadReader.readLine();
+        }
+        int citiesSize = cities.size();
+        int roadsSize = roads.size();
+        provAndCityReader.close();
+        roadReader.close();
+        return cities.get(random.nextInt(citiesSize)) + roads.get(random.nextInt(roadsSize));
+    }
+
+    /**
+     * 随机生成一组中国地址
+     */
+    public static String[] randomChineseAddress(int count) throws Exception{
+        String[] address = new String[count];
+        for (int i = 0; i <count; i++)
+            address[i] = randomChineseAddress();
+        return address;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
---
+++ src/com/bandery/random/RandomChineseName.java
@@ -0,0 +1,62 @@
+package com.bandery.random;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+/*
+ * 随机生成中文用户名
+ * */
+public class RandomChineseName {
+
+    private static final Random random = new Random(System.currentTimeMillis());
+
+    /*
+     * 随机生成中国用户名，姓氏只去常见姓氏，过滤冷僻姓
+     * */
+    public static String randomChineseName() throws Exception{
+        BufferedReader firstNameReader = new BufferedReader(new FileReader(new File("resources/commonChineseFirstName")));
+        BufferedReader chineseReader = new BufferedReader(new FileReader(new File("resources/commonChinese")));
+        String line = firstNameReader.readLine();
+        ArrayList<String> firstNames = new ArrayList<>();
+        ArrayList<String> chinese = new ArrayList<>();
+        while (line != null){
+            firstNames.add(line.trim());
+            line = firstNameReader.readLine();
+        }
+        line = chineseReader.readLine();
+        while (line != null){
+            chinese.add(line.trim());
+            line = chineseReader.readLine();
+        }
+        firstNameReader.close();
+        chineseReader.close();
+        int firstNameSize = firstNames.size();
+        int chineseSize = chinese.size();
+        //随机二字还是三字
+        if(random.nextBoolean())
+            return firstNames.get(random.nextInt(firstNameSize)) + chinese.get(random.nextInt(chineseSize));
+        return firstNames.get(random.nextInt(firstNameSize)) + chinese.get(random.nextInt(chineseSize)) +
+                chinese.get(random.nextInt(chineseSize));
+    }
+
+    /*
+    * 随机生成count个中文姓名
+    * */
+    public static String[] randomChineseName(int count) throws Exception{
+        String[] names = new String[count];
+        for (int i = 0; i < count; i++){
+            names[i] = randomChineseName();
+        }
+        return names;
+    }
+
+    public static void main(String[] args) throws Exception{
+        String[] names = randomChineseName(10);
+        System.out.println(Arrays.toString(names));
+    }
+
+}
